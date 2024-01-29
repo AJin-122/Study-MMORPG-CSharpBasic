b6bdaa2f715dc956bd57c5daf96d9855e1796692
HwangAJin <dkwls1049@gmail.com>
2024-01-29 14:44:25 +0900

    이벤트 내용 추가

--- basicgrammar/MyEvent.cs
+++ basicgrammar/MyEvent.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace basicgrammar
 {
-    internal class MyEvent
+    //이벤트는 직접적으로 호출 할 수 없다.
+    //Observer Pattern
+    class MyEvent
     {
+    }
+
+    class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if(Console.KeyAvailable == false) return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                // 사용자가 A 키를 눌렀을 때 모두한테 알려준다.
+                // InputKey 에 등록된 함수가 실행된다.
+                InputKey();
+            }
+        }
     }
 }
--- basicgrammar/Program.cs
+++ basicgrammar/Program.cs
@@ -19,6 +19,11 @@ namespace basicgrammar
             }
         }
 
+        //이벤트 발생 시 실행 할 함수
+        static void OnInputTest()
+        {
+            Console.WriteLine("Input Received!");
+        }
 
         static void Main(string[] args)
         {
@@ -27,7 +32,7 @@ namespace basicgrammar
             int item = myIntList.GetItem(0);
             MyList<short> myShortList = new MyList<short>();
             */
-            ///* 대리자
+            /* 대리자
             MyDelegate myDelegate = new MyDelegate();
             myDelegate.ButtonPressed(myDelegate.TestDelegate);
 
@@ -36,6 +41,17 @@ namespace basicgrammar
             MyDelegate.OnClicked clicked = new MyDelegate.OnClicked(myDelegate.TestDelegate);
             clicked += myDelegate.TestDelegate2;
             clicked();
+            */
+            ///* 이벤트
+            InputManager inputManager = new InputManager();
+
+            inputManager.InputKey += OnInputTest;
+
+            while (true)
+            {
+                inputManager.Update();
+            }
+            
             // */
         }
     }
